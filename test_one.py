3895b648722c4dc68cdb6cfb5bd065042c88c3ca
sarendar <sarender.vennapureddy@gmail.com>
2019-04-16 15:59:09 +0530

    master commit

--- test_one.py
+++ test_one.py
@@ -6,5 +6,6 @@ b = 30
 if a > b:
     print("THis is first time..")
     print("This is for all branches...")
+    print("This is for only master brqanch....!!!!")
 else:
     print("This is elase condition.....") 
