f1e8e6f476324c73fdf72bf5375e54927b410805
sarendar <sarender.vennapureddy@gmail.com>
2019-04-16 15:17:47 +0530

    verify revert changes

--- test_one.py
+++ test_one.py
@@ -6,5 +6,6 @@ b = 30
 if a > b:
     print("THis is first time..")
     print("For commeted changes....!!!!")
+    print("For commeted changes verification....!!!")
 else:
     print("This is elase condition.....") 
